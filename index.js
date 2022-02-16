87f8c7d527df040a43d2a81bcbcc2f27e7bed2f4
Ryan Hinkle <hinkle.ryan97@gmail.com>
2022-02-16 13:34:46 -0800

    commit tree 9274d18898a0cc370f6fad06f7f964227f1eb9e7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        if (driver.toLowerCase() == name.toLowerCase()) {
+        return driver
+    }
+    })
+}
+
+function fuzzyMatch(drivers) {
+    return drivers.filter(function(driver, par) {
+        if (driver == par) {
+            return driver
+        }
+    })
+}
